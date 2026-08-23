535280260a261b4ea609535e6885d2d141627a57
msabz <36938563+msabz@users.noreply.github.com>
2026-08-23 03:57:37 +0100

    build: use built-in Kotlin in app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,5 @@
-import java.util.Properties
-
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
@@ -37,10 +34,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
     }
 
     packaging {
